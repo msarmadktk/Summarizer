06d7cb487e2191e80a1346f5a8bcdc76779f6437
M Sarmad <muhammad.sarmad.databot@gmail.com>
2024-06-24 16:13:10 +0500

    More flexibility options added

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, render_template
-import summarize  # We'll create this module for summarization
+from flask import Flask, render_template, request
+import summarize
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     summary = ""
-    if request.method == 'POST':
-        article = request.form['article']
-        summary = summarize.summarize_text(article)
-    return render_template('index.html', summary=summary)
+    if request.method == "POST":
+        article = request.form["article"]
+        num_sentences = int(request.form["num_sentences"])
+        algorithm = request.form["algorithm"]
+        summary = summarize.summarize_text(article, num_sentences=num_sentences, algorithm=algorithm)
+    return render_template("index.html", summary=summary)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- summarize.py
+++ summarize.py
@@ -1,11 +1,23 @@
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
 
-def summarize_text(text):
-    if len(text.split()) < 10:  # Ensure the text is long enough
+def summarize_text(text, num_sentences=5, algorithm='lsa'):
+    if len(text.split()) < 10:
         return "Text is too short to summarize"
+    
     parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LsaSummarizer()
-    summary = summarizer(parser.document, 10)  # Summarize the document into 2 sentences
+    
+    if algorithm == 'lsa':
+        summarizer = LsaSummarizer()
+    elif algorithm == 'lex_rank':
+        summarizer = LexRankSummarizer()
+    elif algorithm == 'text_rank':
+        summarizer = TextRankSummarizer()
+    else:
+        summarizer = LsaSummarizer()
+    
+    summary = summarizer(parser.document, num_sentences)
     return ' '.join(str(sentence) for sentence in summary)
